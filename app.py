e56d14afb8b6edbd11fbe8856bfe738e4b8f89a7
GopavarapuDivya <divyagopavarapu2000@gmail.com>
2021-07-20 10:27:17 +0530

    modified

--- app.py
+++ app.py
@@ -32,7 +32,6 @@ def predict():
         filename=secure_filename(file.filename)
         filepath=os.path.join(basepath,'upload/',file.filename)
         file.save(filepath)
-        print(filepath)
         livepreds = model_predict(filepath,model)
         if livepreds==1:
             return render_template('withoutmask.html',filename=filename)
@@ -64,14 +63,13 @@ class VideoCamera(object):
         self.video = cv2.VideoCapture(0)
     def __del__(self):
         self.video.release()
-        os.remove('temp.jpg')
     def get_frame(self):
         ret,img=self.video.read()
         face=face_classifier.detectMultiScale(img,scaleFactor=1.1,minNeighbors=4)
         for(x,y,w,h) in face:
             face_img = img[y:y+h, x:x+w]
-            cv2.imwrite('temp.jpg',face_img)
-            test_image=image.load_img('temp.jpg',target_size=(150,150,3))
+            cv2.imwrite('upload/temp.jpg',face_img)
+            test_image=image.load_img('upload/temp.jpg',target_size=(150,150,3))
             test_image=image.img_to_array(test_image)
             test_image=np.expand_dims(test_image,axis=0)
             pred=model.predict(test_image)[0][0]
